5db2f986d6320e5605ac70bce4615bc54fb6e781
Rahulshahare <rahulshahare@gmail.com>
2024-09-10 05:24:17 +0530

    Basic qr code is generating

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { QRCode } from 'react-qrcode-logo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>QRCode generator</h2>
+      <QRCode
+        value='https://github.com/rahulshahare'
+        size={200}
+        bgColor='#fff'
+        fgColor='#000'
+        eyeRadius={10}
+        qrStyle='dots'
+      />
     </div>
   );
 }
