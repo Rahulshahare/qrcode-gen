811464a3335287e47157d6257c3b5156e4e8b0f7
Rahulshahare <rahulshahare@gmail.com>
2024-09-10 17:22:45 +0530

    Theme is changing, Custome option for creating qr are added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Square from './assets/square-svgrepo-com.svg';
 import RoundedSquare from './assets/black-rounded-square-shape-svgrepo-com.svg';
 import Circle from './assets/circle-svgrepo-com.svg';
 
+import scanMe from './assets/scanMe.png';
+import brown from './assets/brown.png';
+import bluegreen from './assets/bluegreen.png';
+import defaultImg from './assets/default.png';
+
 import faceBookLogo from './socialMediaLogos/faceBookLogo.svg';
 import instagramLogo from './socialMediaLogos/instagramLogo.svg';
 import linkedInLogo from './socialMediaLogos/linkedInLogo.svg';
@@ -23,6 +28,18 @@ function App() {
   const [qrContent, SetqrContent] = useState('tel:727610182000');
   const [qrLogoImage, SetqrLogoImage] = useState(null);
   const [title, SetTitle] = useState('TO CONTACT');
+
+  const [bgColor, SetBgColor] = useState("#ffffff");
+  const [fgColor, SetFgColor] = useState("#000000");
+  const [eyeColor, SetEyeColor] = useState("#000000");
+  const [ ScanME, SetScanME] = useState(false)
+
+  const themeChanger = (bg,fg,eyeC,sc) =>{
+    SetBgColor(bg);
+    SetFgColor(fg);
+    SetEyeColor(eyeC);
+    SetScanME(sc);
+  }
  
   
   
@@ -31,7 +48,7 @@ function App() {
     border: '5px solid #000', // Frame border (customize thickness and color)
     borderRadius: '15px',     // Rounded corners for the frame
     display: 'inline-block',  // Ensure frame fits tightly around the QR code
-    backgroundColor: '#000',  // Background color of the frame
+    backgroundColor: ScanME ? '#000' : '#fff',  // Background color of the frame
     boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.1)',
   };
 
@@ -114,15 +131,15 @@ function App() {
       <div className='preview'>
       <h2>QRCode generator</h2>
         <div ref={qrCodeRef} style={frameStyle} className='frameStyleCss'>
-          <h3>SCAN ME<br/><span>{title}</span></h3>
+          {ScanME && ( <h3>SCAN ME<br/><span>{title}</span></h3>)}
           <QRCode
             value={qrContent}
             size={300}
             ecLevel='M'
-            bgColor='#fff'
-            fgColor='#00796B'
+            bgColor={bgColor}
+            fgColor={fgColor}
             eyeRadius={eyeRadius}
-            eyeColor='#388E3C'
+            eyeColor={eyeColor}
             qrStyle= {qrStyle}
             quietZone={10}
             logoImage={qrLogoImage ? qrLogoImage : undefined}
@@ -160,20 +177,27 @@ function App() {
         </div>
         <div className='qrStyle'>
           <h3>QR Eye Style</h3>
-          <img src={Square} onClick={()=>SeteyeRadius(0)} title="Squaare"/>
-          <img src={RoundedSquare} onClick={()=>SeteyeRadius(5)} title="Rounded Square"/>
-          <img src={Circle} onClick={()=>SeteyeRadius(50)} title="Circle"/>
+          <img src={Square} onClick={()=>SeteyeRadius(0)} title="Square" alt='Square'/>
+          <img src={RoundedSquare} onClick={()=>SeteyeRadius(5)} title="Rounded Square" alt='Rounded Square'/>
+          <img src={Circle} onClick={()=>SeteyeRadius(50)} title="Circle" alt='Circle'/>
         </div>
         <div className='qrLogoImages'>
           <h3>QR Logo images</h3>
           <img src={noImage} onClick={()=>SetqrLogoImage()} title='None'/>
-          <img src={faceBookLogo} onClick={()=>SetqrLogoImage(faceBookLogo)} title='Facebook'/>
-          <img src={instagramLogo} onClick={()=>SetqrLogoImage(instagramLogo)} title='Instagram'/>
-          <img src={linkedInLogo} onClick={()=>SetqrLogoImage(linkedInLogo)} title='LinkedIn'/>
-          <img src={telegramLogo} onClick={()=>SetqrLogoImage(telegramLogo)} title='Telegram'/>
-          <img src={twitterLogo} onClick={()=>SetqrLogoImage(twitterLogo)} title='Twitter'/>
-          <img src={whatsAppLogo} onClick={()=>SetqrLogoImage(whatsAppLogo)} title='WhatsApp'/>
-          <img src={youTubeLogo} onClick={()=>SetqrLogoImage(youTubeLogo)} title='Youtube'/>
+          <img src={faceBookLogo} onClick={()=>SetqrLogoImage(faceBookLogo)} title='Facebook' alt='fb'/>
+          <img src={instagramLogo} onClick={()=>SetqrLogoImage(instagramLogo)} title='Instagram' alt='insta'/>
+          <img src={linkedInLogo} onClick={()=>SetqrLogoImage(linkedInLogo)} title='LinkedIn' alt='linkedIn'/>
+          <img src={telegramLogo} onClick={()=>SetqrLogoImage(telegramLogo)} title='Telegram' alt='Tele'/>
+          <img src={twitterLogo} onClick={()=>SetqrLogoImage(twitterLogo)} title='Twitter' alt='Tw'/>
+          <img src={whatsAppLogo} onClick={()=>SetqrLogoImage(whatsAppLogo)} title='WhatsApp' alt='wht'/>
+          <img src={youTubeLogo} onClick={()=>SetqrLogoImage(youTubeLogo)} title='Youtube' alt='Ytube'/>
+        </div>
+        <div className='themes'>
+          <h3>Pre-made templates</h3>
+          <img src={scanMe} onClick={()=>themeChanger("#ffffff","#00796B","#388E3C", true)}/>
+          <img src={defaultImg} onClick={()=>themeChanger("#ffffff","#000000","#000000", false)}/>
+          <img src={brown} onClick={()=>themeChanger("#000","#FFA000","#FFCD4E", false)}/>
+          <img src={bluegreen} onClick={()=>themeChanger("#ffffff","#00796B","#388E3C", false)}/>
         </div>
       </div>
     </div>
