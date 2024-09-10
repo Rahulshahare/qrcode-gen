abb63c6c20bc80497b6a261541c340660ea72800
Rahulshahare <rahulshahare@gmail.com>
2024-09-10 05:36:51 +0530

    QR code is downloadable

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { QRCode } from 'react-qrcode-logo';
 
 function App() {
+  const qrCodeRef = useRef(null);
+
+  const downloadQRCode = () =>{
+    const canvas = qrCodeRef.current.querySelector('canvas');
+    const url = canvas.toDataURL('image/png');
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'qr-code.png';
+    a.click();
+  }
+
   return (
     <div className="App">
       <h2>QRCode generator</h2>
-      <QRCode
-        value='https://github.com/rahulshahare'
-        size={200}
-        bgColor='#fff'
-        fgColor='#000'
-        eyeRadius={10}
-        qrStyle='dots'
-      />
+        <div ref={qrCodeRef}>
+          <QRCode
+            value='tel:123456'
+            size={200}
+            ecLevel='M'
+            bgColor='#fff'
+            fgColor='#000'
+            eyeRadius={10}
+            qrStyle='dots'
+          />
+        </div>
+        <button onClick={downloadQRCode}>Download QRCode</button>
     </div>
   );
 }
